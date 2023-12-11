2f94bbc6b2fcf1c44cc2e597d0d00008cf7b4822
yyalovenko <yyalovenko@gmail.com>
2023-12-11 18:03:59 +0200

    Change App info

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,6 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My SSH app.");
+        System.out.println("SSH app for Users.");
     }
 }
